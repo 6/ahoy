9dd580fafe29a11c90b50658effbee9f455a51e8
Peter Graham <peterghm@gmail.com>
2015-03-09 22:24:39 -0400

    force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  force_ssl if: :ssl_configured?
 
   def current_user
     @current_user ||= User.find(session[:current_user_id])  if session[:current_user_id]
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def current_user!
     current_user or redirect_to root_path
+  end
+
+  def ssl_configured?
+    !Rails.env.development?
   end
 end
