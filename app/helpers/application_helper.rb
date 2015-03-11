69e122ce47a266a480d4fa4dd1b2944ff76e7adf
Peter Graham <peterghm@gmail.com>
2015-03-11 04:01:59 -0400

    another helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
   def active_if_current(path)
     request.path == path ? ' active ' : ''
   end
+
+  def active_if_any_current(paths)
+    any_active = paths.any? do |path|
+      path = send(path)  if path.is_a?(String)
+      path == request.path
+    end
+    any_active ? ' active ' : ''
+  end
 end
