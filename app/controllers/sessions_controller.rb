8581feccaf798eadad042d4aa90b8e6b51cc25ff
Peter Graham <peterghm@gmail.com>
2015-03-11 02:43:22 -0400

    only allow non-gmail.com addresses

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.from_omniauth!(request.env['omniauth.auth'])
-    session[:current_user_id] = user.id
-    redirect_to root_path
+    auth = request.env['omniauth.auth']
+    if auth.info.email.match(/@(gmail|googlemail)\.com\z/)
+      redirect_to root_path, alert: "Please choose log in with your work Google account (e.g. you@company.com)"
+    else
+      user = User.from_omniauth!(auth)
+      session[:current_user_id] = user.id
+      redirect_to root_path
+    end
   end
 
   def destroy
