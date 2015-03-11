8f1f181b1043d5630261b93484549d85c7429ad4
Peter Graham <peterghm@gmail.com>
2015-03-11 03:26:04 -0400

    simplify and style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def body_classes
+    name = controller_name
+    [
+      "#{name}-#{action_name}",
+      "request-path#{request.path.gsub("/", "-")}",
+    ].join(" ").gsub("_", "-")
+  end
 end
