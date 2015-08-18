a92e142142f22f0ab3f817d259ae5107479c6239
Peter Graham <peter@wealthsimple.com>
2015-08-18 00:37:26 -0400

    set up mailer/sendgrid and move all ENV variables to secrets.yml

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,16 @@ module Ahoy
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_mailer.smtp_settings = {
+      address: "smtp.sendgrid.net",
+      port: 587,
+      domain: "heroku.com",
+      authentication: :plain,
+      user_name: Rails.application.secrets.sendgrid.fetch("username"),
+      password: Rails.application.secrets.sendgrid.fetch("password"),
+    }
+
+    config.action_mailer.default_url_options = { host: Rails.application.secrets.action_mailer.fetch("default_host") }
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret, {
+  provider :google_oauth2, Rails.application.secrets.google_oauth2.fetch("client_id"), Rails.application.secrets.google_oauth2.fetch("client_secret"), {
     name: "google",
     scope: "email,profile",
   }
